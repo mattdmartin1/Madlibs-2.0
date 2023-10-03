bffa29dba2a8df237833696b79cf09c1c9feb7a5
mattdmartin1 <mattdmartin1@gmail.com>
2023-10-03 09:00:22 -0700

    First Commit Monkey King

--- Madlibs 2.0.py
+++ Madlibs 2.0.py
@@ -3,3 +3,17 @@
 # Under those new blank spaces, add labels for parts of speech (noun, verb, adjective, adverb, etc)
 # Have a friend fill in the blanks using their own words.
 # Read your very own story.
+
+import random
+
+verb = input('Please input a verb: ')
+adjective = input('Please input a adjective: ')
+noun = input('Please input a noun: ')
+adverb = input('Please input a adverb: ')
+
+# Story Monkey King
+
+    
+print("\nThe day I saw the Monkey King {} was one of the most interesting days of the year. After he did that," 
+"the king played chess on his \nbrother's {} and then combed his {} hair with a comb made out of old fish bones." 
+" Later that same day, I saw the \nMonkey King dance {} in front of an audience of kangaroos and wombats. \n".format(verb, noun, adjective, adverb))
