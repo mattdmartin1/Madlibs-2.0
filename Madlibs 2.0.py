d21c1594a4a785a313c08d30a41805a08d172941
mattdmartin1 <mattdmartin1@gmail.com>
2023-10-03 08:28:10 -0700

    Created New File

---
+++ Madlibs 2.0.py
@@ -0,0 +1,5 @@
+# Write a story.
+# Erase random words.
+# Under those new blank spaces, add labels for parts of speech (noun, verb, adjective, adverb, etc)
+# Have a friend fill in the blanks using their own words.
+# Read your very own story.
